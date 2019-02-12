c0e056abc5dee81a24878a615c3deffb7502f187
durbaez3 <durbaez3@gmail.com>
2019-02-12 00:41:17 -0400

    close sesion

--- Products/Products/Models/Menu.cs
+++ Products/Products/Models/Menu.cs
@@ -1,11 +1,25 @@
-﻿using System;
-using System.Windows.Input;
-using GalaSoft.MvvmLight.Command;
+﻿
+
 
 namespace Products.Models
 {
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+    using Products.Services;
+    using ViewModels;
+
     public class Menu
     {
+        #region Services
+        NavigationService navigation;
+        #endregion
+
+        #region Constructors
+        public Menu()
+        {
+            navigation = new NavigationService();
+        }
+        #endregion
         #region Properties
         public string Icon { get; set; }
 
@@ -26,7 +40,13 @@ namespace Products.Models
 
         private void Navigate()
         {
-            throw new NotImplementedException();
+            switch (PageName)
+            {
+                case "LoginView":
+                    MainViewModel.GetInstance().Login = new LoginViewModel();
+                    navigation.SetMainPage("LoginView");
+                    break;
+            }
         }
         #endregion
     }
